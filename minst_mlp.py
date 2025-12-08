0284353be57cc6e1bf405499f22c175030532440
Zayed Ahmed <zayed967@yahoo.com>
2025-12-08 14:14:42 -0500

    add visulization of the loss curve and inference file

--- minst_mlp.py
+++ minst_mlp.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 batch_size = 32
 learning_rate = 0.01
 num_epochs = 10
-hidden_size = 128
+hidden_size = 64
 
 # 3. Dataset & DataLoaders
 mean = 0.1307
@@ -40,36 +40,47 @@ test_loader = DataLoader(test_dataset,
 
 # 4. MLP Model
 class SimpleMLP(nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_size):
         super().__init__()
-        self.fc1 = nn.Linear(784, 128)
+        self.fc1 = nn.Linear(784, hidden_size)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(128, 10)
+        self.fc2 = nn.Linear(hidden_size, 32)
+        self.relu = nn.ReLU()
+        self.fc3 = nn.Linear(32, 10)
 
     def forward(self, x):
         x = x.view(x.size(0), -1)  # (batch_size, 1, 28, 28) --> (batch_size, 784)
-        x = self.fc1(x)      # (batch_size, 784) --> (batch_size, 128)
-        x = self.relu(x)     # (batch_size, 128) --> (batch_size, 128)
-        logits = self.fc2(x) # (batch_size, 128) --> (batch_size, 10)
+        x = self.fc1(x)      # (batch_size, 784) --> (batch_size, hidden_size)
+        x = self.relu(x)     # (batch_size, hidden_size) --> (batch_size, hidden_size)
+        x = self.fc2(x)      # (batch_size, hidden_size) --> (batch_size, 32)
+        x = self.relu(x)     # (batch_size, 32) --> (batch_size, 32)
+        logits = self.fc3(x) # (batch_size, 32) --> (batch_size, 10)
 
         return logits
 
 
 # 5. Loss & Optimizer
-model = SimpleMLP()
+model = SimpleMLP(hidden_size)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 # 6. Training Loop
+losses = []
+epoch_losses = []
 for epoch in range(num_epochs):
+    running_loss = 0
     for batch_idx, (images, labels) in enumerate(train_loader):
         optimizer.zero_grad()
         logits = model(images)
         loss = criterion(logits, labels)
+        losses.append(loss.item())
+        running_loss += loss.item()
         loss.backward()
         optimizer.step()
         if batch_idx % 100 == 0:
             print(f"Epoch {epoch+1}, Batch {batch_idx}, Loss: {loss.item():.4f}")
+    epoch_loss = running_loss / len(train_loader)
+    epoch_losses.append(epoch_loss)
 
 # 7. Evaluation
 model.eval()
@@ -85,5 +96,20 @@ with torch.no_grad():
 accuracy = correct / total
 print(f"Accuracy: {accuracy:.4f}")
 
+# visualize the loss curve 
+import matplotlib.pyplot as plt
 
-        
+plt.plot(epoch_losses)
+plt.title("Epoch Loss Curve")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.show()
+
+plt.plot(losses)
+plt.title("Training Loss Curve")
+plt.xlabel("Batch Number")
+plt.ylabel("Loss")
+plt.show()
+
+# 8. Save model
+torch.save(model.state_dict(), "mnist_mlp.pth")

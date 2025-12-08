71a3bc4e3b6331da87601f672a35aaa08ff3e400
Zayed Ahmed <zayed967@yahoo.com>
2025-12-08 14:18:19 -0500

    rename typo and guard training with main entery

--- inference.py
+++ inference.py
@@ -1,12 +1,12 @@
 
-from minst_mlp import SimpleMLP, test_dataset
+from mnist_mlp import SimpleMLP, test_dataset
 import torch
 
 loaded_model = SimpleMLP(64)
 loaded_model.load_state_dict(torch.load("mnist_mlp.pth"))
 loaded_model.eval()
 
-image, label = test_dataset[0]      # image: (1, 28, 28)
+image, label = test_dataset[1]      # image: (1, 28, 28)
 image = image.unsqueeze(0)          # (1, 1, 28, 28) -> add batch dim
 
 with torch.no_grad():
